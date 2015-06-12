505808c5762c915ae72d2b953df967bfdb9b12ff
Andrew Appleton <andrew@floatleft.com>
2015-06-12 14:29:17 -0700

    Apps route

---
+++ tests/unit/routes/apps-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:apps', 'Unit | Route | apps', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
